0fbf61df9022e4ee29d57ee8a3c8ac0825aac61c
Farida <faridah.absi@gmail.com>
2018-06-25 10:36:29 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,32 +1,40 @@
-const codes = [
-  "ArrowUp",
-  "ArrowUp",
-  "ArrowDown",
-  "ArrowDown",
-  "ArrowLeft",
-  "ArrowRight",
-  "ArrowLeft",
-  "ArrowRight",
-  "b",
-  "a"
-];
+var code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
-var index = 0
+// keep track of index outside of the event handler
+var index = 0;
+//console.log(index);
 
 function init() {
-  
-  document.body.addEventListener('keydown', function(e){
-    const key = parseInt(e.detail || e.which)
+  // your code here
+  var body = document.body;
+  body.addEventListener('keydown', function(e){
+    var key = parseInt(e.detail || e.which);
+
+    // check if key is equal to current value of index in code array
+    // then loop through the array if conditions match
 
     if (key === code[index]) {
-      index++
-   
+      // if yes, increment index by 1
+      index++;
+
+      // if still in the loop, check if index is equal to code array length
       if (index === code.length - 1) {
-        index = 0
-        alert("YOU DID IT!")
+        // reset the index counter
+        index = 0;
+        // if yes, you passed, congrats!
+        alert("Congrats!");
       }
-    } else {
-      index = 0
-    }
-  })
+
+
+
+  // if key doesn't match current index in code array, then reset the index to zero
+  } else {
+    index = 0;
+  }
+
+
+  });
+
 }
+
+//init();
