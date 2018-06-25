63d3403763ef05b1ae1342830afd7ce592a5bd46
Farida <faridah.absi@gmail.com>
2018-06-25 10:37:36 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,40 +1,24 @@
-var code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+if (window.addEventListener) {
+    var index = 0;
+    var konami = [38,38,40,40,37,39,37,39,66,65,13];
 
-// keep track of index outside of the event handler
-var index = 0;
-//console.log(index);
+    window.addEventListener("keydown", function(e){
+        if (e.keyCode === konami[index])
+        {
+            index++; //valid key at the valid point
 
-function init() {
-  // your code here
-  var body = document.body;
-  body.addEventListener('keydown', function(e){
-    var key = parseInt(e.detail || e.which);
-
-    // check if key is equal to current value of index in code array
-    // then loop through the array if conditions match
-
-    if (key === code[index]) {
-      // if yes, increment index by 1
-      index++;
-
-      // if still in the loop, check if index is equal to code array length
-      if (index === code.length - 1) {
-        // reset the index counter
-        index = 0;
-        // if yes, you passed, congrats!
-        alert("Congrats!");
-      }
-
-
-
-  // if key doesn't match current index in code array, then reset the index to zero
-  } else {
-    index = 0;
-  }
-
-
-  });
-
+            if (index == konami.length)
+            {
+                alert("Correct");
+            } else {
+                alert("Keep going");
+            }
+        } else {
+            // incorrect code restart
+            index = 0;
+            alert("Wrong"); 
+        }
+   });
 }
 
 //init();
